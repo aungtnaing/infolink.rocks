271128d15b5de8999e1d805801266500c3518a66
aungtnaing <aungtnaing82@gmail.com>
2017-05-06 21:05:10 +0630

    hel

--- resources/views/includes/mainslidesmyanmar.blade.php
+++ resources/views/includes/mainslidesmyanmar.blade.php
@@ -6,7 +6,7 @@
                  @foreach($mainslides as $mainslide)
                 <li class="first-slide" data-transition="fade" data-slotamount="10" data-masterspeed="300">
                     <img src="{{ $mainslide->photourl1 }}" data-fullwidthcentering="on" alt="slide">
-             		<div class="tp-caption first-line lfr tp-resizeme start" data-x="center" data-hoffset="0" data-y="center" data-speed="2500" data-start="500" data-easing="Power4.easeOut" data-splitin="none" data-splitout="none" data-elementdelay="0" data-endelementdelay="0"><span>Info-link project / {{ $mainslide->created_at }}</span><h1 class="mfbody">{{ $mainslide->mname }} <em class="mfbody">{{ $mainslide->msubtitle }}</em></h1><div class="slider-button"><a href="#">View Project</a></div></div>
+             		<div class="tp-caption first-line lfr tp-resizeme start" data-x="center" data-hoffset="0" data-y="center" data-speed="2500" data-start="500" data-easing="Power4.easeOut" data-splitin="none" data-splitout="none" data-elementdelay="0" data-endelementdelay="0"><span>Info-link project / {{ $mainslide->created_at }}</span><h1 style="color:black" class="mfbody">{{ $mainslide->mname }} <em class="mfbody">{{ $mainslide->msubtitle }}</em></h1><div class="slider-button"><a href="#">View Project</a></div></div>
                 </li>
                 @endforeach
             
